ede7da63f2314609baa3d575f144df6fa8536791
Joys <joys.tower@gmail.com>
2016-05-10 12:29:04 -0500

    WKT service

--- api/app/Http/Routes/GeoDynamic/dyn.intersect.php
+++ api/app/Http/Routes/GeoDynamic/dyn.intersect.php
@@ -2,10 +2,10 @@
 
 /**
   * @SWG\Get(
-  *     path="/dyn/dw?s={schema}&t={table}&c={columns}&w={column_filter}:{value_filter}&lat={latitude}&lng={longitud}&mts={meters}",
-  *     summary="Distance Within service analisys",
-  *     description="Servicio de analisis que regresa las geometrias con la informacion solicitada",
-  *     operationId="dw",
+  *     path="/dyn/intersect?s={schema}&t={table}&c={columns}&w={column_filter}:{value_filter}&wkt={wkt}&mts={meters}",
+  *     summary="Obtener lo que intersecte",
+  *     description="Servicio de analisis que regresa las geometrias con la informacion solicitada segun el WKT enviado como parametro",
+  *     operationId="intersect",
   *     tags={"Distance Within"},
   *     produces={"application/json"},
   *     @SWG\Parameter(
@@ -52,7 +52,7 @@
   * 		   	name="wkt",
   * 			  in="path",
   * 			  required=true,
-  * 			  type="number",
+  * 			  type="string",
   * 			  description="WKT geometry format",
   *         default="POINT(-102.2827363014221 21.85996530350067)",
   *     ),
@@ -67,18 +67,18 @@
   *     @SWG\Response(
   *         response=200,
   *         description="successful operation",
-  *         @SWG\Schema(ref="#/dw")
+  *         @SWG\Schema(ref="#/intersect")
   *     ),
   * )
   */
-Route::get('/dw', function(){
+Route::get('/intersect', function(){
   $response = array();
 
   $s = Input::get('s', 'public');
   $t = Input::get('t', '');
   $c = Input::get('c', 'gid');
   $w = Input::get('w', '');
-  $lat = Input::get('wkt', '');
+  $wkt = Input::get('wkt', '');
   $mts = Input::get('mts', 0);
 
 
@@ -91,8 +91,12 @@ Route::get('/dw', function(){
   $GEOM_CUT_LINE = "ST_AsGeoJSON( ( (ST_Dump(ST_Intersection(S.geom, A.geom))).geom )::geometry)::json As geometry";
   $GEOM_INTERSECT = "ST_AsGeoJSON(A.geom)::json As geometry";
   $WHERE = "";
-  $GEOM_WKT_SPLIT = "st_buffer(ST_SetSRID(ST_Point($lng, $lat),4326) , $mts)";
-  $GEOM_WKT_WHERE = "ST_DWithin(A.geom, ST_SetSRID(ST_Point($lng, $lat),4326), $mts)";
+  $GEOM_WKT_SPLIT = " ST_GeomFromText( '$wkt', 4326 ) ";
+  $GEOM_WKT_WHERE = " ST_intersects( A.geom, ST_GeomFromText('$wkt', 4326 ) )";
+  if (strpos( strtolower($wkt), 'point') !== false) {
+    $GEOM_WKT_SPLIT = "st_buffer( ST_GeomFromText( '$wkt', 4326 ) , $mts)";
+    $GEOM_WKT_WHERE = "ST_DWithin( A.geom, ST_GeomFromText( '$wkt', 4326 ), $mts)";
+  }
 
   switch ($TBL) {
     case 'inegi.rnc_red_vial_2015':
@@ -104,6 +108,12 @@ Route::get('/dw', function(){
         $SPLIT = " WITH split AS ( SELECT ($GEOM_WKT_SPLIT)::geometry geom ) ";
         $TBL = "inegi.inter15_vias As A, split S";
         $GEOM = $GEOM_CUT_LINE;
+      break;
+    case 'inegi.denue_2016':
+        $c = "nom_estab, raz_social, codigo_act, nombre_act, per_ocu, tipo_vial, nom_vial, tipo_v_e_1, nom_v_e_1,
+              tipo_v_e_2, nom_v_e_2, tipo_v_e_3, nom_v_e_3, numero_ext, letra_ext, edificio, edificio_e, numero_int,
+              letra_int, tipo_asent, nomb_asent, tipocencom, nom_cencom, num_local, cod_postal, cve_ent, entidad,
+              cve_mun, municipio, cve_loc, localidad, ageb, manzana, telefono, correoelec, www, tipounieco, fecha_alta, ";
       break;
     default:
         $TBL = $TBL." As A";
@@ -125,7 +135,7 @@ Route::get('/dw', function(){
   $rs = DB::select($sql,[]);
   $geo = array2GeoJSON($rs);
 
-  if($c !== 'gid'){
+  if($c !== 'gid' && $TBL!='inegi.denue_2016'){
     foreach($rs as $ro){
       $r = (array) $ro;
       if(isset($INFO[$r[$c]])){
@@ -142,15 +152,4 @@ Route::get('/dw', function(){
     "sql" => $sql
   ]);
 
-
-
-  /*return Response::json([
-    "s"=>$s,
-    "t"=>$t,
-    "c"=>$c,
-    "w"=>$w,
-    "lat"=>$lat,
-    "lng"=>$lng,
-    "mts"=>$mts
-  ]);*/
 });
--- api/app/Http/Routes/GeoDynamic/dyn.php
+++ api/app/Http/Routes/GeoDynamic/dyn.php
@@ -6,7 +6,7 @@ Route::group(['prefix'=>'dyn', 'before' => 'oauth', 'middleware' => 'cors'], fun
   require app_path('Http/Routes/GeoDynamic/dyn.apidoc.php');
   //Catalogos
   require app_path('Http/Routes/GeoDynamic/dyn.catalog.php');
-  //Distance within
-  require app_path('Http/Routes/GeoDynamic/dyn.dw.php');
+  //Intersect
+  require app_path('Http/Routes/GeoDynamic/dyn.intersect.php');
 
 });
