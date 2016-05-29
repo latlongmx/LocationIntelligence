327689f39a82b432504d0205308272edd89c65e2
joystor <joys.tower@gmail.com>
2016-05-28 23:35:43 -0500

    Fix load mapscritp

--- api/app/Http/Routes/GeoDynamic/dyn.pb_wms.php
+++ api/app/Http/Routes/GeoDynamic/dyn.pb_wms.php
@@ -35,19 +35,22 @@ where
   and pea not in('N/D','*') and pea is not null;
 */
       //$q = "SELECT $COL FROM inegi.pobviv2010 where ST_Intersects(geom,ST_MakeEnvelope('$WKT', 4326))";
+
+
+
       $q = "select p.entidad || p.mun || p.loc || p.ageb || p.mza cvegeo, $COL variab
       from inegi.censo_resageburb_2010 P,
        inegi.mgn_estados E
       where
         ST_Intersects(E.geom,ST_MakeEnvelope($BOX, 4326))
         and E.cve_ent = P.entidad;";
-      $rs = DB::select($q,[]);
+      $rs = DB::select($q,[])->toArray();
       foreach($rs as $r){
-        $v = $r->variab; //[$COL];
+        $v = $r[$COL];
         if(is_numeric($v) && $v > $MAXVAL){
           $MAXVAL = (int)$v;
         }
-        $VALUES[] = array($r->cvegeo, $v);
+        $VALUES[] = array($r["cvegeo"], $v);
       }
   }
 
