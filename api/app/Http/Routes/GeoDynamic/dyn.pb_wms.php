5f2bb478c534dcc8ca872799ff4e31f7337573af
joystor <joys.tower@gmail.com>
2016-05-30 23:35:36 -0500

    Fix WMS

--- api/app/Http/Routes/GeoDynamic/dyn.pb_wms.php
+++ api/app/Http/Routes/GeoDynamic/dyn.pb_wms.php
@@ -3,6 +3,8 @@
 
 Route::get('/pb_wms', //['middleware' => 'oauth', function() {
 function(){
+  $date = date('m/d/Y h:i:s a', time());
+  Log::info("Map start:".$date);
   //$userId = Authorizer::getResourceOwnerId();
   $MAP = getMapObjConfig();
 
@@ -36,7 +38,8 @@ where
 group by p.entidad;
 */
       //$q = "SELECT $COL FROM inegi.pobviv2010 where ST_Intersects(geom,ST_MakeEnvelope('$WKT', 4326))";
-
+      $date = date('m/d/Y h:i:s a', time());
+      Log::info("Start Max:".$date);
       //MAXIMOS
       $q = "select p.entidad ent, max($COL) maximo
       from inegi.censo_resageburb_2010 P,
@@ -51,6 +54,9 @@ group by p.entidad;
         $MAXVALS[] = array("ent" => $r->ent, "max" =>$r->maximo);
       }
 
+      $date = date('m/d/Y h:i:s a', time());
+      Log::info("Finish Max:".$date);
+
 
       /*$q = "select E.cvegeo cvegeo, $COL variab
       from inegi.censo_resageburb_2010 P,
@@ -63,7 +69,8 @@ group by p.entidad;
         $VALUES[] = array("cvegeo" => $r->cvegeo, "variable" => $r->variab);
       }*/
   }
-
+  $date = date('m/d/Y h:i:s a', time());
+  Log::info("ConfigLayer:".$date);
   $LAY = getLayerObjConfig($MAP, 'Manzanas', $COL);
   #$LAY->set('data', "geom from (select gid, cvegeo, geom from inegi.inter15_manzanas where ST_Intersects(geom,!BOX!)) as T using unique gid using srid=4326");
   $qry_data = "geom from (
@@ -100,7 +107,8 @@ group by p.entidad;
     }
     $style->set('opacity',100);
   }*/
-
+  $date = date('m/d/Y h:i:s a', time());
+  Log::info("Config Styles:".$date);
   foreach ($MAXVALS as $mx){
     $MAXVALUE = (int)$mx["max"];
     $ENT = $mx["ent"];
@@ -123,11 +131,16 @@ group by p.entidad;
       $i++;
     }
   }
+  $date = date('m/d/Y h:i:s a', time());
+  Log::info("End Styles:".$date);
 
   $class = new \ClassObj( $LAY );
   $style = new \StyleObj( $class );
   $style->color->setHex('#ffff99');
   $style->set('opacity',100);
+
+  $date = date('m/d/Y h:i:s a', time());
+  Log::info("Dispatch:".$date);
 
   ms_ioinstallstdouttobuffer();
   $map_file = storage_path("logs/ms_file.map");
@@ -142,6 +155,8 @@ group by p.entidad;
       header('Content-type: application/xml');
       echo $buffer;
     }else{
+      $date = date('m/d/Y h:i:s a', time());
+      Log::info("Resolv:".$date); 
       header('Content-type: $contenttype');
       ms_iogetStdoutBufferBytes();
     }
