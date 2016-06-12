f1b9c8878e0bb07f39b9b7e6fc3221681b993625
joystor <joys.tower@gmail.com>
2016-06-12 12:55:18 -0500

    Add icon file to locations

--- api/app/Http/Routes/Workspace/ws.icon.get.php
+++ api/app/Http/Routes/Workspace/ws.icon.get.php
@@ -41,12 +41,12 @@ Route::get('/icon', ['middleware' => 'oauth', function() {
   $icoName = Input::get('nm', '');
   $path = storage_path() . '/pins/'.$userId.'/'.$icoName;
 
-  /*if(!File::exists($path)) abort(404);
+  if(!File::exists($path)) abort(404);
 
   $file = File::get($path);
   $type = File::mimeType($path);
 
-  $response = Response::make($file, 200);
+  /*$response = Response::make($file, 200);
   $response->header("Content-Type", $type);
   return $response;*/
 
