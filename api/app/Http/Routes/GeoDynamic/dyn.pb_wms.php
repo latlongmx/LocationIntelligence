99d5d40e29e3dece591243a815f35dbd3e25ea92
joystor <joys.tower@gmail.com>
2016-06-12 20:06:02 -0500

    Fix WSM color distribution

--- api/app/Http/Routes/GeoDynamic/dyn.pb_wms.php
+++ api/app/Http/Routes/GeoDynamic/dyn.pb_wms.php
@@ -21,17 +21,22 @@ function(){
   if($BOX!= ""){
 
       //MAXIMOS
-      $q = "select p.entidad ent, max($COL) maximo
+      $q = "select p.entidad ent, p.mun, max($COL::int) maximo
         from inegi.censo_resageburb_2010 P,
          inegi.mgn_estados E
         where
           ST_Intersects(E.geom,ST_MakeEnvelope($BOX, 4326))
           and E.cve_ent = P.entidad
           and $COL not in('N/D','*') and $COL is not null
-        group by p.entidad;";
+          and mun <> '000'
+          and loc <> '0000'
+          and ageb <> '0000'
+          and mza <> '000'
+        group by p.entidad, p.mun
+        order by p.entidad, p.mun;";
       $rs = DB::select($q,[]);
       foreach($rs as $r){
-        $MAXVALS[] = array("ent" => $r->ent, "max" =>$r->maximo);
+        $MAXVALS[] = array("ent" => $r->ent, "mun" => $r->mun, "max" =>$r->maximo);
       }
   }
 
@@ -58,17 +63,35 @@ function(){
 
 
   //Configurar Layer con puntos de las manzanas (ST_PointOnSurface)
-  $LAY_MZA = getLayerObjConfig($MAP, 'Manzanas_Points');
+  /*$LAY_MZA = getLayerObjConfig($MAP, 'Manzanas_Points');
   $qry_data = "geom from (select gid, entidad ent, ".
       "(CASE WHEN $COL not in('N/D','*') and $COL is not null THEN $COL::int ELSE 0 END)::int AS pbvar, ".
       " ST_PointOnSurface(geom) geom from inegi.pobviv2010 where ST_Intersects(geom,!BOX!) ) as T using unique gid using srid=4326";
   $LAY_MZA->set('data', $qry_data);
-  $LAY_MZA->set('type', MS_LAYER_POINT);
+  $LAY_MZA->set('type', MS_LAYER_POINT);*/
+
+  //Layer Manzanas Sin Estilo
+  $LAY_SinStyle = getLayerObjConfig($MAP, 'Manzanas_sin');
+  $qry_data = "geom from (select gid, entidad ent, mun, ".
+      "(CASE WHEN $COL not in('N/D','*') and $COL is not null THEN $COL::int ELSE 0 END)::int AS pbvar, ".
+      "geom from inegi.pobviv2010 where ST_Intersects(geom,!BOX!) ) as T using unique gid using srid=4326";
+  $LAY_SinStyle->set('data', $qry_data);
+  $LAY_SinStyle->set('type', MS_LAYER_POLYGON);
+  $LAY_SinStyle->set('labelitem', "pbvar");
+  $class = new \ClassObj( $LAY_SinStyle );
+  $style = new \StyleObj( $class );
+  $style->color->setHex("#ffff99");
+  $style->set("opacity",100);
+  $label = new \labelObj();
+  $label->color->setRGB(0,0,0);
+	$label->set("size", 8);
+  $class->addLabel($label);
+	//$class->label->set("font", "arial");
 
 
   //Layer Manzanas
   $LAY = getLayerObjConfig($MAP, 'Manzanas');
-  $qry_data = "geom from (select gid, entidad ent, ".
+  $qry_data = "geom from (select gid, entidad ent, mun, ".
       "(CASE WHEN $COL not in('N/D','*') and $COL is not null THEN $COL::int ELSE 0 END)::int AS pbvar, ".
       "geom from inegi.pobviv2010 where ST_Intersects(geom,!BOX!) ) as T using unique gid using srid=4326";
   $LAY->set('data', $qry_data);
@@ -78,6 +101,7 @@ function(){
   foreach ($MAXVALS as $mx){
     $MAXVALUE = (int)$mx["max"];
     $ENT = $mx["ent"];
+    $MUN = $mx["mun"];
     $GG = round($MAXVALUE/$GROUPS);
 
     $r=$MAXVALUE;
@@ -86,7 +110,7 @@ function(){
     while($i<=$GROUPS){ #$MAXVALUE){
       $r2 = $GG*$i;
       $class = new \ClassObj( $LAY );
-      $class->setExpression("([pbvar] < ".$r2." && '[ent]'='".$ENT."')");
+      $class->setExpression("([pbvar] < ".$r2." && '[ent]'='".$ENT."' && '[mun]'='".$MUN."')");
       $style = new \StyleObj( $class );
       $ncol = ((($i*100)/$GROUPS)*0.01);
       $col = getColorFromColToCol("ffff99", "ff0000", $ncol );
