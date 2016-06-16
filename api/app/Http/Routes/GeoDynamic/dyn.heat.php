52e75d4f331d24b9456c822dc35cc04b3f65e8b2
joystor <joys.tower@gmail.com>
2016-06-16 00:11:03 -0500

    generate heat data

--- api/app/Http/Routes/GeoDynamic/dyn.heat.php
+++ api/app/Http/Routes/GeoDynamic/dyn.heat.php
@@ -36,8 +36,17 @@ Route::get('/heat', ['middleware' => 'oauth', function() {
 
   $filter = Input::get('filter', '');
   $wkt = Input::get('wkt', '');
-
-  $sql = "SELECT st_xmax(geom) lat, st_ymax(geom) lng, 1.0 value, D.cve_ent
+/*
+SELECT st_xmax(D.geom) lat, st_ymax(D.geom) lng, 1.0 val_data, D.cve_ent
+FROM inegi.denue_2016 D
+LEFT JOIN inegi.mgn_estados E
+ON E.cve_ent = D.cve_ent
+WHERE
+ST_Intersects(E.geom, ST_GeomFromText( 'POLYGON((-99.15652513504028 19.416957838501798,-99.13919806480408 19.416957838501798,-99.12187099456787 19.416957838501798,-99.12187099456787 19.4253255113588,-99.12187099456787 19.4336931842158,-99.13919806480408 19.4336931842158,-99.15652513504028 19.4336931842158,-99.15652513504028 19.4253255113588,-99.15652513504028 19.416957838501798))', 4326 ) )
+and D.nom_estab ilike '%oxxo%'
+limit 10;
+*/
+  $sql = "SELECT st_xmax(D.geom) lat, st_ymax(D.geom) lng, 1.0 val_data, D.cve_ent
     FROM inegi.denue_2016 D
     LEFT JOIN inegi.mgn_estados E
     ON E.cve_ent = D.cve_ent
